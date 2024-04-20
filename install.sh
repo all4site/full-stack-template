d09d96fae354625532aa54804881b068abcc99b3
all4site <all4sitecomua@gmail.com>
2024-04-20 19:09:44 +0300

    wget for installing

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ Green='\033[0;32m'
 NC='\033[0m'
 
 echo "${Green}Installing...${NC}"
+git clone https://github.com/all4site/full-stack-template.git &&
 cd install-scrips &&
 chmod +x *.sh &&
 ./front.sh &&
