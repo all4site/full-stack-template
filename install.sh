a1a1b49b9f331c97b75ae4ff38e534b836a08e8b
all4site <all4sitecomua@gmail.com>
2024-04-20 19:16:22 +0300

    change folder

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 Green='\033[0;32m'
 NC='\033[0m'
+FOLDER='full-stack-template'
 
 echo "${Green}Installing...${NC}"
 git clone https://github.com/all4site/full-stack-template.git &&
-cd install-scrips &&
+cd ${FOLDER}/install-scrips &&
 chmod +x *.sh &&
 ./front.sh &&
 echo "${Green}Finish...${NC}"
