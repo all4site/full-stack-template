5fe5eaeaa3687dde138dc44e97bea1a70e5215b2
all4site <all4sitecomua@gmail.com>
2024-04-20 19:33:05 +0300

    fix path way

--- install-scripts/front.sh
+++ install-scripts/front.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
 Green='\033[0;32m'
 NC='\033[0m'
+FOLDER='full-stack-template'
 
 echo "${Green}Installing Front...${NC}"
-cd ../ && mkdir front && cd front &&
+cd ../../ && mkdir front && cd front &&
 npx create-next-app . &&
 npm i -D prettier @trivago/prettier-plugin-sort-imports &&
-cd ../install-data/front &&
-cp -rf *  ../../front &&
+cd ../${FOLDER}/install-data/front &&
+cp -rf *  ../../../front &&
 echo "${Green}Finish Front...${NC}"
