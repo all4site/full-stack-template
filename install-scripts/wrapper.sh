717456f89c7fb81ecce9988d03bd15dbf18c30b7
all4site <all4sitecomua@gmail.com>
2024-04-22 20:20:34 +0300

    fix

--- install-scripts/wrapper.sh
+++ install-scripts/wrapper.sh
@@ -4,9 +4,9 @@ NC='\033[0m'
 FOLDER='full-stack-template'
 
 echo "${Green}Installing Wrapper...${NC}"
-cd ${FOLDER}/install-data/ &&
+cd ../install-data/ &&
 cp -rf  .gitignore ../../ &&
 cp -rf package.json ../../ &&
 cd ../../ &&
-#rm -rf install.sh full-stack-template
+rm -rf install.sh full-stack-template
 echo "${Green}Finish Wrapper...${NC}"
