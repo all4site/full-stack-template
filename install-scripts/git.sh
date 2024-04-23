e9a7cd442010192527193edac3933d44bc58457e
all4site <all4sitecomua@gmail.com>
2024-04-23 13:52:45 +0300

    git.sh

---
+++ install-scripts/git.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+Green='\033[0;32m'
+NC='\033[0m'
+FOLDER='full-stack-template'
+
+echo "${Green}Git Installing...${NC}"
+cd ../../ &&
+git init &&
+git add . &&
+git commit -m "first commit" &&
+git branch -M main &&
+read -p "Git link: " link &&
+git remote add origin $link &&
+git push -u origin main &&
+echo "${Green}Git Finish...${NC}"
+
