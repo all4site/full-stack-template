e6efa3dc4603aff70c628a9243b30f5660504fc8
all4site <all4sitecomua@gmail.com>
2024-04-23 14:33:46 +0300

    fix

--- install-scripts/git.sh
+++ install-scripts/git.sh
@@ -9,7 +9,8 @@ git init &&
 git add . &&
 git commit -m "first commit" &&
 git branch -M main &&
-vared -p 'git remote add origin: ' $link &&
+vared -p 'git remote add origin: ' link &&
+git remote add origin $link &&
 git push -u origin main &&
 echo "${Green}Git Finish...${NC}"
 
