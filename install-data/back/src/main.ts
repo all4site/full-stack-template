de5665b48fa846bcd56a9b5dc076af66ab144fd6
all4site <all4sitecomua@gmail.com>
2024-05-26 18:53:19 +0300

    add server defaut setup

--- install-data/back/src/main.ts
+++ install-data/back/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3001)
+	app.setGlobalPrefix('api')
+	app.useGlobalPipes(new ValidationPipe())
+	app.enableCors({ origin: '*' })
+	await app.listen(4000)
+	console.log('server running on port 4000')
 }
 
 bootstrap()
